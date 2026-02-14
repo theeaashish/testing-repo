91bde69b683f52cf977cec6a4473788df10bae1d
Aashish Joshi <theeaashish@gmail.com>
2026-02-14 22:46:20 +0530

    test

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app  = express();
+
+
+app.get("/", (req, res) => {
+res.json({
+	message: "hey"
+	)}
+});
+
+
+app.listen(3000);
